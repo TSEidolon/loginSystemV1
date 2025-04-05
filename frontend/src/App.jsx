98fb42b70b114a245c34697c861256fb1329fd7b
Edgar Francis Caballero <efrcaballero@gmail.com>
2025-04-04 22:05:06 -0600

    added routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,23 @@
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router';
+import LoginPage from './components/loginPage.jsx'
+import MainPage from './components/mainPage'
+import RegisterPage from './components/registerPage.jsx'
 
 
 
 function App() {
-  
+ 
 
-  return (
-    <div className="text-4xl">
-      hello
-    </div>
-  )
+return (
+<Router>
+  <Routes>
+    <Route path="/" element={<Navigate to="/login" replace />} />
+    <Route path="/login" element={<LoginPage />} />
+    <Route path="/register" element={<RegisterPage />} />
+    <Route path="/main" element={<MainPage />} />
+  </Routes>
+</Router>
+)
 }
 
 export default App
