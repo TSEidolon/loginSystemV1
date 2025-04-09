3dd108e79df783f9d35b2cf07a884fd67867d63c
Edgar Francis Caballero <efrcaballero@gmail.com>
2025-04-08 23:57:41 -0600

    started styling

--- frontend/src/components/loginPage.jsx
+++ frontend/src/components/loginPage.jsx
@@ -38,17 +38,19 @@ const LoginPage = () => {
       <form className="p-6 bg-gray-200 rounded" onSubmit={()=> {handleLogin(); handleLoginAlternative()}}>
         <h2 className="text-2xl mb-4">Login</h2>
         <div className="flex flex-col">
-          <label htmlFor="username">Username</label>
-          <input className="p-2 mb-2" placeholder="User" onChange={(e) => setUsername(e.target.value)} />
+          <label htmlFor="username">Military-ID</label>
+          <input className="p-2 mb-2" placeholder="Username" onChange={(e) => setUsername(e.target.value)} />
         </div>
         <div className="flex flex-col">
           <label htmlFor="username">Password</label>
           <input className="p-2 mb-2" type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
         </div>
-        <button className="bg-blue-500 text-white p-2 rounded" type="submit">Login</button>
-        
+        <p>mini catchphrase here</p>
+        <div>
+          <button className="bg-blue-500 text-white p-2 rounded" type="submit">Login</button>
+          <button className="bg-blue-500 text-white p-2 rounded" type="button" onClick={navigateRegister}>Register</button>
+        </div>
       </form>
-      <button className="bg-blue-500 text-white p-2 rounded" type="button" onClick={navigateRegister}>Register</button>
       <button className="bg-blue-500 text-white p-2 rounded" type="button" onClick={navigateGuest}>Guest</button>
     </div>
   );
