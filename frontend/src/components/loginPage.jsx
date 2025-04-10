cfc7ea9c57d7b1c6b8d362850717dfd4002dacdc
Edgar Francis Caballero <efrcaballero@gmail.com>
2025-04-10 02:23:54 -0600

    added 3d functionality. will fix file paths next time to be more organized

--- frontend/src/components/loginPage.jsx
+++ frontend/src/components/loginPage.jsx
@@ -1,8 +1,23 @@
-import { useState } from "react";
+import { useState, Suspense } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import Eagle from "../assets/images/eagle.png"
 import Global from "../assets/images/global.png"
+import HeroCamera from "./heroCamera";
+
+import { Canvas } from '@react-three/fiber'
+import { Environment, Stars, PerspectiveCamera, useTexture, OrbitControls } from '@react-three/drei'
+
+function ImagePlane({ url }) {
+  const texture = useTexture(url);
+
+  return (
+    <mesh>
+      <circleGeometry args={[2.5, 90]} />
+      <meshBasicMaterial map={texture} />
+    </mesh>
+  );
+}
 
 const LoginPage = () => {
   const [username, setUsername] = useState("");
@@ -27,7 +42,7 @@ const LoginPage = () => {
   }
 
   const handleLoginAlternative = () => {
-    window.alert("Server not found fool")
+    window.alert("Unable To Connect to F.A.S Servers")
   }
 
 
@@ -35,17 +50,30 @@ const LoginPage = () => {
     navigate("/main");
   }
 
+
+
   return (
     
-    <div className="flex flex-col gap-5 justify-center items-center h-full lg:h-screen ">
-      <h2 className="text-[3rem]">Service Gurantees Citizenship</h2>
+    <div className="login-background flex flex-col gap-5 justify-center items-center h-full lg:h-screen ">
+      <h2 className="text-[3rem] text-[var(--tertiary-color)]">Service Gurantees Citizenship</h2>
       <main className="bg-[var(--tertiary-color)] border-black border-2 p-7 flex rounded-md gap-7 lg:w-[1222px] text-[var(--primary-color)]">
         <section className="login-left h-[600px] w-[700px] flex justify-center">
-          <span className="bg-[var(--quarternary-color)] size-full rounded-md border-black border-2 flex justify-center items-center">model here</span>
+          <Canvas className=" size-full rounded-md border-black border-2">
+            <Suspense>
+              <OrbitControls makeDefault />
+              <ambientLight intensity={1}/>
+              <directionalLight position={[10,10,10]} intensity={0.5}/>
+              <HeroCamera>
+                <ImagePlane url="/images/logo.png" /> 
+                <Stars saturation={0} count={400} speed={0.5} />
+              </HeroCamera>
+              <color attach="background" args={['#092A21']} />
+            </Suspense>
+          </Canvas>
         </section>
         <section className="login-right lg:w-[450px] flex flex-col items-center justify-between ">
           <h2 className="text-4xl ">Federal Armed Services</h2>
-          <form className="p-6 bg-[var(--tertiary-color)] border-2 border-black rounded w-full h-[440px] flex justify-center flex-col gap-7 lg:text-xl relative " onSubmit={()=> {handleLogin(); handleLoginAlternative()}}>
+          <form className="p-6 bg-[var(--tertiary-color)] ring-2 ring-black shadow-md shadow-black  rounded w-full h-[440px] flex justify-center flex-col gap-7 lg:text-xl relative " onSubmit={()=> {handleLogin(); handleLoginAlternative()}}>
             <h2>Login to Database</h2>
             <div className="flex flex-col">
               <label htmlFor="username">Military-ID</label>
@@ -60,8 +88,8 @@ const LoginPage = () => {
               <button className="bg-[var(--quarternary-color)] border-2 border-black   w-[145px] p-2 rounded-sm" type="submit">Login</button>
               <button className="bg-[var(--quarternary-color)] border-2 border-black w-[145px] p-2 rounded-sm" type="button" onClick={navigateRegister}>Register</button>
             </div>
-            <img className="absolute top-[3%] right-[5%] z-10 size-[80px]" src={Eagle} alt="" />
-            <img className="absolute bottom-[-6%] right-[44%] z-10 size-[50px]" src={Global} alt="" />
+            <img className="absolute top-[4%] right-[5%] z-10 size-[80px]" src={Eagle} alt="" />
+            <img className="absolute bottom-[6%] right-[44%] z-10 size-[50px]" src={Global} alt="" />
           </form>
           <button className="bg-[var(--quarternary-color)] border-2 border-black text-3xl p-2 rounded w-full" type="button" onClick={navigateGuest}>Guest Login</button>
         </section>
@@ -69,6 +97,7 @@ const LoginPage = () => {
     </div>
   );
 }
+
 
 
 export default LoginPage
