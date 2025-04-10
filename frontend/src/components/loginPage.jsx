3f0534ce610f892e63c6dc2c7a5cc9d4dbcd1921
Edgar Francis Caballero <efrcaballero@gmail.com>
2025-04-09 23:23:16 -0600

    edited styling

--- frontend/src/components/loginPage.jsx
+++ frontend/src/components/loginPage.jsx
@@ -37,29 +37,33 @@ const LoginPage = () => {
     
     <div className="flex flex-col gap-5 justify-center items-center h-full lg:h-screen ">
       <h2 className="text-[3rem]">Service Gurantees Citizenship</h2>
-      <main className="border-black border-2 p-7 flex rounded-md gap-7 lg:w-[1222px]">
+      <main className="bg-[var(--tertiary-color)] border-black border-2 p-7 flex rounded-md gap-7 lg:w-[1222px] text-[var(--primary-color)]">
         <section className="login-left h-[600px] w-[700px] flex justify-center">
-          <span className="size-full rounded-md border-black border-2 flex justify-center items-center">model here</span>
+          <span className="bg-[var(--quarternary-color)] size-full rounded-md border-black border-2 flex justify-center items-center">model here</span>
         </section>
         <section className="login-right lg:w-[450px] flex flex-col items-center justify-between ">
-          <h2 className="text-4xl text-[var(--primary-color)]">Federal Armed Services</h2>
-          <form className="p-6 bg-gray-200 rounded w-full h-[440px] flex justify-center flex-col gap-7 lg:text-xl" onSubmit={()=> {handleLogin(); handleLoginAlternative()}}>
+          <h2 className="text-4xl ">Federal Armed Services</h2>
+          <form className="p-6 bg-[var(--tertiary-color)] border-2 border-black rounded w-full h-[440px] flex justify-center flex-col gap-7 lg:text-xl relative " onSubmit={()=> {handleLogin(); handleLoginAlternative()}}>
             <h2>Login to Database</h2>
             <div className="flex flex-col">
               <label htmlFor="username">Military-ID</label>
-              <input className="p-2 mb-2 border-black border-2" placeholder="Username" onChange={(e) => setUsername(e.target.value)} />
+              <input className="bg-[var(--quarternary-color)] p-2 mb-2 border-black border-2" placeholder="Username" onChange={(e) => setUsername(e.target.value)} />
             </div>
             <div className="flex flex-col">
               <label htmlFor="username">Password</label>
-              <input className="p-2 mb-2 border-black border-2" type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
+              <input className="bg-[var(--quarternary-color)] p-2 mb-2 border-black border-2" type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
             </div>
             <p className="text-center">I'm doing my part</p>
             <div className="flex justify-between">
-              <button className="bg-blue-500  w-[145px] text-white p-2 rounded-sm" type="submit">Login</button>
-              <button className="bg-blue-500 w-[145px] text-white p-2 rounded-sm" type="button" onClick={navigateRegister}>Register</button>
+              <button className="bg-[var(--quarternary-color)] border-2 border-black   w-[145px] p-2 rounded-sm" type="submit">Login</button>
+              <button className="bg-[var(--quarternary-color)] border-2 border-black w-[145px] p-2 rounded-sm" type="button" onClick={navigateRegister}>Register</button>
             </div>
+
+              <img className="absolute z-10 size-[100px]" src="" alt="" />
+              <img className="absolute z-10 size-[100px]" src="" alt="" />
+
           </form>
-          <button className="bg-blue-500 text-white text-3xl p-2 rounded w-full" type="button" onClick={navigateGuest}>Guest Login</button>
+          <button className="bg-[var(--quarternary-color)] border-2 border-black text-3xl p-2 rounded w-full" type="button" onClick={navigateGuest}>Guest Login</button>
         </section>
       </main>
     </div>
