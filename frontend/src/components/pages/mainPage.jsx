290e5775cd93d35e9a33712daa2d4e5b51ad3761
Edgar Francis Caballero <efrcaballero@gmail.com>
2025-04-30 19:36:56 -0600

    edited styling

--- frontend/src/components/pages/mainPage.jsx
+++ frontend/src/components/pages/mainPage.jsx
@@ -78,14 +78,14 @@ const MainPage = () => {
         <img className="absolute  top-0 left-[6%] w-[700px] " src={Divider} alt="" />
         <section className=" w-[850px] h-[600px] relative flex flex-col border-2 border-black shadow-sm shadow-black rounded-md">
           <div className="button-area absolute z-10 -right-11 top-4 flex flex-col gap-5">
-            <button className="h-[60px] w-[90px] rounded-md border-2 border-black bg-blue-400 " onClick={() => {showInfo(1); checkClick()}}>
-              <IoIosStats className="size-full p-2"/>
+            <button className="h-[60px] w-[90px] rounded-md bg-[var(--tertiary-color)] shadow-sm shadow-black" onClick={() => {showInfo(1); checkClick()}}>
+              <IoIosStats className=" size-full p-2"/>
             </button>
-            <button className="h-[60px] w-[90px] rounded-md border-2 border-black bg-blue-400 " onClick={() => {showInfo(2); checkClick()}}>
-              <GiPistolGun className="size-full p-2"/>
+            <button className="h-[60px] w-[90px] rounded-md bg-[var(--tertiary-color)] shadow-sm shadow-black" onClick={() => {showInfo(2); checkClick()}}>
+              <GiPistolGun className=" size-full p-2"/>
             </button>
-            <button className="h-[60px] w-[90px] rounded-md border-2 border-black bg-blue-400 " onClick={() => {showInfo(3); checkClick()}}>
-              <FaBook className="size-full p-2"/>
+            <button className="h-[60px] w-[90px] rounded-md bg-[var(--tertiary-color)] shadow-sm shadow-black" onClick={() => {showInfo(3); checkClick()}}>
+              <FaBook className=" size-full p-2"/>
             </button>
           </div>
           <div className="text-area font-semibold h-[85%] text-[var(--primary-color)]">
